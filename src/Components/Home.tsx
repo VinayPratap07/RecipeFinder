91563f869ed21360f309952b5dfe27d424363a1e
VinayPratap07 <vpstanwar07@gmail.com>
2025-09-04 11:07:37 +0530

    Added parallel query to home page

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -1,43 +1,49 @@
-import { useQuery } from '@tanstack/react-query'
+import { useQueries } from '@tanstack/react-query'
 import { Card } from './Card';
 import Loader from './Loader/Loader';
 import Error from './Error/Error';
 import { getRandomMeal } from '../APICalls/GetApi';
 import type { Meal } from '../APICalls/ApiRespone';
 
-async function fetchRecipe() {
-    const randomMeal = await getRandomMeal();
-    return randomMeal.data.meals || [];
-  }
 
 
 function Home() {  
 
-  const { isLoading, error, data } = useQuery<Meal[]>({
-    queryKey: ['recipeSearch'],
-    queryFn: fetchRecipe,
-    staleTime: 1000,
-    refetchOnWindowFocus: false,
-    refetchOnMount: false,
+  const NUM_RANDOM = 50;
+  const queries = useQueries({
+    queries: Array.from({ length: NUM_RANDOM }, (_, i) => ({
+      queryKey: ["randomMeal", i], 
+      queryFn: async (): Promise<Meal> => {
+        const res = await getRandomMeal();
+        return res.data.meals[0]; 
+      },
+      staleTime: 1000,
+      refetchOnWindowFocus: false,
+      refetchOnMount: false,
+    })),
   });
 
-  if (isLoading) {
-    return <div><Loader/></div>
-  }
-  if (error) {
-    return <div><Error/></div>
-  }
-
   return (
-    <div className="HomeDiv">
-      {data && data?.length > 0 ? (
-        data.map((meal) => (
-          <Card key={meal.idMeal} img={meal.strMealThumb} title={meal.strMeal} id={meal.idMeal}/>
-        ))
-        
-      ) : (
-        <div>No meals found</div>
-      )}
+ <div className="HomeDiv">
+      {queries.map((q, i) => {
+        if (q.isLoading) {
+          return <Loader key={i} />;   // show loader for just this one
+        }
+        if (q.error) {
+          return <Error key={i} />;    // show error just for this one
+        }
+        if (q.data) {
+          return (
+            <Card
+              key={q.data.idMeal}
+              img={q.data.strMealThumb}
+              title={q.data.strMeal}
+              id={q.data.idMeal}
+            />
+          );
+        }
+        return null;
+      })}
     </div>
   );
 }
